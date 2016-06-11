1ccfe8573a88be7052722c25a52688f1a57e1f39
wangsd <shidewang@outlook.com>
2016-06-11 22:40:34 +0800

    add disqus

--- tool.py
+++ tool.py
@@ -18,7 +18,10 @@ def gen_pages():
 
     global pages
 
-    pages = '''pages: \n  - [index.md, 首页]\n'''
+    fmt = "- [%32s, %16s]"
+
+    pages = "pages: \n  " + fmt + "\n" 
+    pages = pages % ("index.md", "首页")
 
     dirs = {
         r"./articles/":"文章",
@@ -26,7 +29,7 @@ def gen_pages():
         r"./translate/":"翻译",
     }
 
-    pages_fmt = "  - [%s, %s]\n"
+    pages_fmt = "  " + fmt + "\n"
 
     for path in dirs:
         for md in glob(path + "/*.md"):
@@ -34,7 +37,8 @@ def gen_pages():
             if path in md:
                 pages = pages + pages_fmt % (md, dirs[path])
 
-    pages = pages + "  - [about.md, 关于]\n"
+    pages = pages + "  " + fmt + "\n" 
+    pages = pages % ("about.md", "关于")
 
 
 
